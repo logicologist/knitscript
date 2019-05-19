5d7c645c07749664955d816b9b60332f03904d35
Sarah Marshall <marshallsaraha@gmail.com>
2019-05-18 22:38:03 -0700

    Clean up type annotations in astnodes

--- knitscript/astnodes.py
+++ knitscript/astnodes.py
@@ -1,48 +1,52 @@
 from abc import ABC, abstractmethod
 from itertools import chain
-from typing import Any, List
+from typing import Generic, List, TypeVar
 
 from knitscript.stitch import Stitch
 
+T = TypeVar("T")
+Stitches = List[Stitch]
+Rows = List[Stitches]
 
-class Expr(ABC):
+
+class Expr(ABC, Generic[T]):
     @abstractmethod
-    def eval(self) -> Any:
+    def eval(self) -> T:
         pass
 
 
-class StitchExpr(Expr):
+class StitchExpr(Expr[Stitches]):
     def __init__(self, stitch: Stitch) -> None:
         self._stitch = stitch
 
-    def eval(self) -> List[Stitch]:
+    def eval(self) -> Stitches:
         return [self._stitch]
 
 
-class RowExpr(Expr):
-    def __init__(self, exprs: List[Expr]) -> None:
+class RowExpr(Expr[Rows]):
+    def __init__(self, exprs: List[Expr[Stitches]]) -> None:
         self._exprs = exprs
 
-    def eval(self) -> List[List[Stitch]]:
+    def eval(self) -> Rows:
         return [flat_eval(self._exprs)]
 
 
-class RepeatExpr(Expr):
-    def __init__(self, exprs: List[Expr], count: int) -> None:
-        self._exprs = exprs
-        self._count = count
-
-    def eval(self) -> List[Any]:
-        return flat_eval(self._exprs) * self._count
-
-
-class PatternExpr(Expr):
-    def __init__(self, rows: List[Expr]) -> None:
+class PatternExpr(Expr[Rows]):
+    def __init__(self, rows: List[Expr[Rows]]) -> None:
         self._rows = rows
 
-    def eval(self) -> List[List[Stitch]]:
+    def eval(self) -> Rows:
         return flat_eval(self._rows)
 
 
-def flat_eval(exprs: List[Expr]) -> List[Any]:
+class RepeatExpr(Expr[List[T]]):
+    def __init__(self, exprs: List[Expr[List[T]]], count: int) -> None:
+        self._exprs = exprs
+        self._count = count
+
+    def eval(self) -> List[T]:
+        return flat_eval(self._exprs) * self._count
+
+
+def flat_eval(exprs: List[Expr[List[T]]]) -> List[T]:
     return list(chain.from_iterable(map(lambda e: e.eval(), exprs)))
