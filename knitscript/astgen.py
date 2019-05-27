0930e77aadeba19348dea676a0f882afac7d177f
Sarah Marshall <saraham@cs.washington.edu>
2019-05-27 12:45:41 -0700

    Add stitch repeats to grammar

--- knitscript/astgen.py
+++ knitscript/astgen.py
@@ -1,6 +1,8 @@
 from functools import singledispatch
+from typing import Collection, Union
 
-from knitscript.astnodes import Expr, StitchExpr, RowExpr, PatternExpr
+from knitscript.astnodes import ExpandingStitchRepeatExpr, Expr, \
+    FixedStitchRepeatExpr, NaturalLit, PatternExpr, RowExpr, StitchExpr
 from knitscript.parser.KnitScriptParser import KnitScriptParser, \
     ParserRuleContext
 from knitscript.stitch import Stitch
@@ -18,15 +20,44 @@ def build_ast(ctx: ParserRuleContext) -> Expr:
 
 
 @build_ast.register
-def _(stitch: KnitScriptParser.StitchContext) -> Expr:
-    return StitchExpr(Stitch.from_symbol(stitch.symbol.text))
+def _(pattern: KnitScriptParser.PatternContext) -> Expr:
+    return PatternExpr(list(map(build_ast, pattern.rows)))
 
 
 @build_ast.register
 def _(row: KnitScriptParser.RowContext) -> Expr:
-    return RowExpr(list(map(build_ast, row.stitches)))
+    return RowExpr(list(map(build_ast, row.stitchList().stitches)))
 
 
 @build_ast.register
-def _(pattern: KnitScriptParser.PatternContext) -> Expr:
-    return PatternExpr(list(map(build_ast, pattern.rows)))
+def _(repeat: KnitScriptParser.StitchRepeatContext) -> Expr:
+    return build_ast(repeat.fixedStitchRepeat() or
+                     repeat.expandingStitchRepeat() or
+                     repeat.stitch())
+
+
+@build_ast.register
+def _(fixed: KnitScriptParser.FixedStitchRepeatContext) -> Expr:
+    count = NaturalLit(int(fixed.count.text))
+    return FixedStitchRepeatExpr(list(map(build_ast, _stitches(fixed))),
+                                 count)
+
+
+@build_ast.register
+def _(expanding: KnitScriptParser.ExpandingStitchRepeatContext) -> Expr:
+    to_last = NaturalLit(int(expanding.toLast.text) if expanding.toLast else 0)
+    return ExpandingStitchRepeatExpr(
+        list(map(build_ast, _stitches(expanding))),
+        to_last
+    )
+
+
+@build_ast.register
+def _(stitch: KnitScriptParser.StitchContext) -> Expr:
+    return StitchExpr(Stitch.from_symbol(stitch.ID().getText()))
+
+
+def _stitches(ctx: Union[KnitScriptParser.FixedStitchRepeatContext,
+                         KnitScriptParser.ExpandingStitchRepeatContext]) \
+        -> Collection[ParserRuleContext]:
+    return [ctx.stitch()] if ctx.stitch() else ctx.stitchList().stitches
