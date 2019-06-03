97d3a7bdced799f1f182e5b86a9f505ab66d727e
Sarah Marshall <saraham@cs.washington.edu>
2019-06-03 00:24:39 -0700

    Update compile_text to export_text in test.py

--- test.py
+++ test.py
@@ -1,25 +1,16 @@
-import sys
-from typing import Optional
-from enum import Enum
-
-from antlr4 import CommonTokenStream, FileStream, StdinStream
+from antlr4 import CommonTokenStream, FileStream
 
 from knitscript.astgen import build_ast
-from knitscript.astnodes import Document, PatternDef, PatternExpr, \
-    pretty_print
-from knitscript.interpreter import alternate_sides, compile_text, flatten, \
-    infer_counts, infer_sides, substitute
-from knitscript.verifiers import verify_pattern
+from knitscript.astnodes import Document, PatternDef, PatternExpr
+from knitscript.export import export_text
+from knitscript.interpreter import alternate_sides, flatten, infer_counts, \
+    infer_sides, substitute
 from knitscript.parser.KnitScriptLexer import KnitScriptLexer
 from knitscript.parser.KnitScriptParser import KnitScriptParser
+from knitscript.verifiers import verify_pattern
+
 
 def process_pattern(filename: str) -> PatternExpr:
-    """
-    Prints the knitting instructions for a KnitScript pattern which is read
-    from the filename or stdin if no filename is provided.
-
-    :param filename: the filename of the KnitScript pattern to run
-    """
     lexer = KnitScriptLexer(
         FileStream(filename)
     )
@@ -35,43 +26,45 @@ def process_pattern(filename: str) -> PatternExpr:
     pattern = infer_counts(infer_sides(substitute(env["main"], env)))
     pattern = flatten(pattern)
     pattern = alternate_sides(pattern)
+    assert isinstance(pattern, PatternExpr)
     return pattern
 
-    print(compile_text(pattern))
-    assert isinstance(pattern, PatternExpr)
-    print(*verify_pattern(pattern), sep="\n")
 
 def check_output(filename: str, expected: str) -> bool:
-	pattern = process_pattern(filename)
-	actual = compile_text(pattern)
-	return actual == expected
+    pattern = process_pattern(filename)
+    actual = export_text(pattern)
+    return actual == expected
+
 
 def verify_error(filename: str) -> bool:
-	pattern = process_pattern(filename)
-	verification = verify_pattern(pattern)
-	errors = []
-	for error in verification:
-		errors.append(error.message)
-	# We can also do more checking of the specific errors... not sure what the right format is, though
-	return len(errors) >= 1
+    pattern = process_pattern(filename)
+    verification = verify_pattern(pattern)
+    errors = []
+    for error in verification:
+        errors.append(error.message)
+    # We can also do more checking of the specific errors... not sure what
+    # the right format is, though
+    return len(errors) >= 1
+
 
 def test(callback, desc: str):
     try:
-        assert(callback())
+        assert callback()
     except AssertionError:
         print("TEST FAILED: " + desc)
     else:
         print("√")
 
 
-
-test(lambda: verify_error("test/too-many-stitches.ks"), "Should catch expedted stitches greater than available")
-test(lambda: verify_error("test/too-few-stitches.ks"), "Should catch expected stitches smaller than available")
-test(lambda: check_output("test/just-right.ks", "CO 12.\nK 12.\n*BO; rep from * to end."), "Should compile simple pattern")
-test(lambda: verify_error("test/double-expanding-repeat-2.ks"), "Should catch bad double expanding repeat")
-test(lambda: not verify_error("test/double-expanding-repeat.ks"), "Should allow okay double expanding repeat") # technically legit even though but nobody writes patterns this way
-
-
-
-
-
+test(lambda: verify_error("test/too-many-stitches.ks"),
+     "Should catch expedted stitches greater than available")
+test(lambda: verify_error("test/too-few-stitches.ks"),
+     "Should catch expected stitches smaller than available")
+test(lambda: check_output("test/just-right.ks",
+                          "CO 12.\nK 12.\n*BO; rep from * to end."),
+     "Should compile simple pattern")
+test(lambda: verify_error("test/double-expanding-repeat-2.ks"),
+     "Should catch bad double expanding repeat")
+# technically legit even though nobody writes patterns this way
+test(lambda: not verify_error("test/double-expanding-repeat.ks"),
+     "Should allow okay double expanding repeat")
