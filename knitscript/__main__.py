43354b2f89de7815d5eb3dc14542ad4c91c708ea
Sarah Marshall <saraham@cs.washington.edu>
2019-06-15 16:07:31 -0700

    Better error messages for file not found/missing filename

--- knitscript/__main__.py
+++ knitscript/__main__.py
@@ -9,10 +9,13 @@ def main() -> None:
     Prints the knitting instructions for a KnitScript pattern which is read
     from a file, or stdin if no filename is provided.
     """
-    try:
-        load_file(sys.argv[1] if len(sys.argv) >= 2 else None, sys.stdout)
-    except InterpretError as e:
-        print(f"error: {e}")
+    if len(sys.argv) != 2:
+        print(f"usage: knitscript <filename>")
+    else:
+        try:
+            load_file(sys.argv[1], sys.stdout)
+        except (IOError, InterpretError) as e:
+            print(f"error: {e}")
 
 
 if __name__ == "__main__":
