431a4cf54707d0a468fefc65fcda98a336cf4054
Sarah Marshall <saraham@cs.washington.edu>
2019-06-16 02:12:31 -0700

    Add save current file feature to editor

--- knitscript/editor/app.py
+++ knitscript/editor/app.py
@@ -1,3 +1,4 @@
+import os
 from functools import wraps
 from io import StringIO
 from tkinter import BOTH, DISABLED, END, FLAT, Menu, NORMAL, NSEW, Text, Tk, \
@@ -37,6 +38,7 @@ class Application(Frame):
         menu = Menu(master)
         file_menu = Menu(menu, tearoff=0)
         file_menu.add_command(label="Open", command=window.open)
+        file_menu.add_command(label="Save", command=window.save)
         file_menu.add_command(label="Save As", command=window.save_as)
         menu.add_cascade(label="File", menu=file_menu)
         master.configure(menu=menu)
@@ -57,8 +59,15 @@ class _Window(Frame):
         self.grid_columnconfigure(2, weight=1)
 
         self._editor = _Editor(self, width=500, height=500)
-        self._editor.grid(row=0, column=0, sticky=NSEW)
         self._editor.text = _DEFAULT_DOCUMENT
+        self._editor.grid(row=0, column=0, sticky=NSEW)
+        self._file = None
+        self._title = "New Document"
+
+        def on_modified(_event):
+            self._modified = self._editor.modified
+
+        self._editor.bind("<<Modified>>", on_modified)
 
         sep = Separator(self)
         sep.grid(row=0, column=1)
@@ -68,18 +77,55 @@ class _Window(Frame):
 
     def open(self) -> None:
         """Opens a file using a file dialog."""
-        file = filedialog.askopenfile(filetypes=_FILE_TYPES)
+        file = filedialog.askopenfile("r+", filetypes=_FILE_TYPES)
         if file is not None:
-            self._editor.text = file.read()
-            file.close()
+            if self._file is not None:
+                self._file.close()
+            self._file = file
+            self._editor.text = self._file.read()
+            self._title = os.path.basename(self._file.name)
+            self._modified = False
+
+    def save(self) -> None:
+        """
+        Saves the current file if there is one. Otherwise, opens the save as
+        dialog.
+        """
+        if self._file is None:
+            self.save_as()
+        else:
+            self._file.seek(0)
+            self._file.write(self._editor.text)
+            self._file.truncate()
+            self._modified = False
 
     def save_as(self) -> None:
         """Saves a file using a file dialog."""
         file = filedialog.asksaveasfile(defaultextension=_EXTENSION,
                                         filetypes=_FILE_TYPES)
         if file is not None:
-            file.write(self._editor.text)
-            file.close()
+            self._title = os.path.basename(file.name)
+            self._file = file
+            self.save()
+
+    @property
+    def _modified(self) -> bool:
+        return self._editor.modified
+
+    @_modified.setter
+    def _modified(self, value) -> None:
+        if value != self._editor.modified:
+            self._editor.modified = value
+        self._title = self._title
+
+    @property
+    def _title(self) -> str:
+        return self.__title
+
+    @_title.setter
+    def _title(self, value) -> None:
+        self.__title = value
+        self.master.title(self.__title + ("*" if self._modified else ""))
 
 
 class _Editor(Frame):
@@ -99,6 +145,13 @@ class _Editor(Frame):
         self._text.bind("<Key>",
                         lambda _event: self.event_generate("<<Typed>>"))
 
+        def bind_modified():
+            self._text.edit_modified(False)
+            self._text.bind("<<Modified>>",
+                            lambda _event: self.event_generate("<<Modified>>"))
+
+        self.after_idle(bind_modified)
+
     @property
     def text(self) -> str:
         """The text in the editor."""
@@ -107,7 +160,18 @@ class _Editor(Frame):
     @text.setter
     def text(self, value: str) -> None:
         self._text.replace("1.0", END, value)
-        self.event_generate("<<Changed>>")
+        self.event_generate("<<Set>>")
+
+    @property
+    def modified(self) -> bool:
+        """
+        Whether the editor has been modified since the last save.
+        """
+        return self._text.edit_modified()
+
+    @modified.setter
+    def modified(self, value) -> None:
+        self._text.edit_modified(value)
 
 
 class _Preview(Frame):
@@ -131,7 +195,7 @@ class _Preview(Frame):
         self._text.pack(expand=YES, fill=BOTH)
 
         self._source = source
-        self._source.bind("<<Changed>>", self._update)
+        self._source.bind("<<Set>>", self._update)
         self._source.bind("<<Typed>>", _debounce(master, 500)(self._update))
         self._update()
 
